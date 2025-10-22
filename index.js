83accb09ddbcd147c41b7d80abeda23cbca48e85
Andre Pires <erdnapfaserip@gmail.com>
2025-10-22 10:50:58 -0300

    feat: POST using multer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+let bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// POST with multer
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded"})
+  }
 
+  const { originalname, mimetype, size } = req.file;
 
+  res.json({ 
+    name: originalname,
+    type: mimetype, 
+    size: size 
+  });
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
